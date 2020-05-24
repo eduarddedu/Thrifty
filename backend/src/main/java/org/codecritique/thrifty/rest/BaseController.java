df42b65225af8cab14859669fc28465135790253
Eduard Dedu <eduardcdedu@gmail.com>
2020-05-24 16:04:36 +0300

    Backend: delete BaseController
    Spring Rest does not seem to support
    inheritance by a resource class of the
    path request mapping defined in a superclass

--- backend/src/main/java/org/codecritique/thrifty/rest/BaseController.java
+++
@@ -1,6 +0,0 @@
-package org.codecritique.thrifty.rest;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/rest-api")
-public abstract class BaseController { }
--- backend/src/main/java/org/codecritique/thrifty/rest/ExpensesController.java
+++ backend/src/main/java/org/codecritique/thrifty/rest/ExpensesController.java
@@ -1,18 +1,18 @@
 package org.codecritique.thrifty.rest;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class ExpensesController extends BaseController {
+@RequestMapping("/rest-api/expenses")
+public class ExpensesController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/expenses")
-    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @GetMapping()
+    public String getAllExpenses() {
         return "Hello";
     }
 }
