bac39cf0106d1c7b318c87e6ad6889afc90cc7e9
Eduard Dedu <eduardcdedu@gmail.com>
2020-05-24 10:27:02 +0300

    Backend: add skeleton rest api classes

---
+++ backend/src/main/java/org/codecritique/thrifty/rest/BaseController.java
@@ -0,0 +1,6 @@
+package org.codecritique.thrifty.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api")
+public abstract class BaseController { }
---
+++ backend/src/main/java/org/codecritique/thrifty/rest/HelloController.java
@@ -0,0 +1,18 @@
+package org.codecritique.thrifty.rest;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController extends BaseController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello";
+    }
+}
