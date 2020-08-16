d9a8a78b0c7c4aa38c2a773723895fbff8f58477
Eduard Dedu <eduardcdedu@gmail.com>
2020-08-16 10:56:42 +0300

    Add assertions in testUpdateLabel to check consistency

--- backend/src/test/java/org/codecritique/thrifty/dao/LabelServiceBeanTest.java
+++ backend/src/test/java/org/codecritique/thrifty/dao/LabelServiceBeanTest.java
@@ -1,19 +1,26 @@
 package org.codecritique.thrifty.dao;
 
+import org.codecritique.thrifty.entity.Category;
+import org.codecritique.thrifty.entity.Expense;
 import org.codecritique.thrifty.entity.Label;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.Iterator;
+import java.util.*;
 
-import static org.codecritique.thrifty.TestUtils.labelSupplier;
-import static org.codecritique.thrifty.TestUtils.randomName;
+import static org.codecritique.thrifty.TestUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LabelServiceBeanTest extends BaseServiceBeanTest {
 
     @Autowired
     private LabelServiceBean labelService;
+
+    @Autowired
+    private ExpenseServiceBean expenseService;
+
+    @Autowired
+    private CategoryServiceBean categoryService;
 
     @Test
     void testAddLabel() {
@@ -42,18 +49,37 @@ class LabelServiceBeanTest extends BaseServiceBeanTest {
 
     @Test
     void testUpdateLabel() {
+        //setup
+
         Label label = labelSupplier.get();
         labelService.store(label);
+
+        Category category = categorySupplier.get();
+        categoryService.store(category);
+
+        Expense expense = expenseSupplier.get();
+        expense.setCategory(category);
+        expense.addLabel(label);
+        expenseService.store(expense);
+
+        //exercise
 
         label.setName(randomName.get());
         labelService.update(label);
 
+        //verify
+
         assertEquals(label, labelService.get(label.getId()));
+
+        //verify the view from expense side is consistent
+        assertEquals(1, expense.getLabels().size());
+        assertTrue(expense.getLabels().contains(label));
+        assertTrue(expenseService.get(expense.getId()).getLabels().contains(label));
     }
 
     @Test
-    void removeLabel() {
-        Label label = new Label("Foo");
+    void testRemoveLabel() {
+        Label label = new Label(randomName.get());
         labelService.store(label);
         assertNotNull(labelService.get(label.getId()));
         labelService.remove(label.getId());
