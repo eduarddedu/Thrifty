6f64b34bb72c17d6a06258bee8685c1051f238ad
Eduard Dedu <eduardcdedu@gmail.com>
2020-10-10 10:04:12 +0300

    Set Java time zone to UTC
    Fix LocalDate minus one day problem

--- backend/src/main/java/org/codecritique/thrifty/Application.java
+++ backend/src/main/java/org/codecritique/thrifty/Application.java
@@ -2,13 +2,21 @@ package org.codecritique.thrifty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@Configuration
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
