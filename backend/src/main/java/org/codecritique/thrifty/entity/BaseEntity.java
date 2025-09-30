a710ec94e0c78cc7950c7f393093a0b2a9d9285d
Eduard Dedu <eduardcdedu@gmail.com>
2025-09-30 11:46:09 +0300

    Use Lombok getter

--- backend/src/main/java/org/codecritique/thrifty/entity/BaseEntity.java
+++ backend/src/main/java/org/codecritique/thrifty/entity/BaseEntity.java
@@ -1,21 +1,20 @@
 package org.codecritique.thrifty.entity;
 
+import lombok.Getter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
-
-    public Long getId() {
-        return id;
-    }
 
     public abstract Long getAccountId();
 
